e55ab7246749503b2c61bd9dff7270c7e35be3b6
bilel <bilel@DESKTOP-93AHQ9K>
2018-07-30 02:13:57 +0100

    bilel commit 02:13

--- z1-Sondage/src/main/java/tn/isi/web/User2Controller.java
+++ z1-Sondage/src/main/java/tn/isi/web/User2Controller.java
@@ -3,6 +3,7 @@ package tn.isi.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,22 @@ import tn.isi.dao.UserRepository;
 import tn.isi.entites.User;
 
 @RestController
+@CrossOrigin("*")
 public class User2Controller {
 	@Autowired
 	private UserRepository userrepository;
-	
-	@RequestMapping(value="/users",method=RequestMethod.GET)
-	public List<User> getListUserByNom(@RequestParam(name="mc",defaultValue="")String mc){
-		return userrepository.chercherUserByNom("%"+mc+"%");
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	/*
+	 * 
+	 * public List<User>
+	 * getListUserByNom(@RequestParam(name="mc",defaultValue="")String mc){ return
+	 * userrepository.chercherUserByNom("%"+mc+"%");
+	 */
+
+	public List<User> getUsers() {
+
+		return userrepository.findAll();
 	}
 
 }
